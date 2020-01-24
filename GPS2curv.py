a89fbf1bffc20273f1da303a82f40dc04d061c01
Waltteri Koskinen <45742869+rasori@users.noreply.github.com>
2020-01-24 21:55:02 +0200

    v1.0

--- GPS2curv.py
+++ GPS2curv.py
@@ -1,25 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # This project goal was to perform conversion from gps location data to curvature file needed in ChassisSim. The project
 # was started when data logger on Tampere Formula Student -19 (TFS19) could not provide accurate acceleration data for
 # ChassisSim software.
 #
 # Creator: Waltteri Koskinen
-# v0.1 23.01.2020
+# v1.0 24.01.2020
 
 import numpy as np
 from matplotlib import pyplot as plt
+import argparse
 
-INPUT = "Toijala.txt"
-filename = 'Toijala_curv.TXT'
+parser = argparse.ArgumentParser()
+parser.add_argument('input', help='state input file')
+parser.add_argument('-o', '--output', action='store', required=False, metavar='', help='state output filename')
+parser.add_argument('-p', '--plot', action='store_true', help='plots the track and curvature vectors in it')
+args = parser.parse_args()
+
 
 def gps2meter(long, lat):
     """
     Function takes long and lat coordinates and converts them to meters in cartesian coordinate system.
     First data point is threaded as an origo.
 
-    :param long: Longitude coordinates
-    :param lat: Latitude coordinates
-    :return: x ,y coordinates in a lists as meters
+    :param long: Longitude coordinates list
+    :param lat: Latitude coordinates list
+    :return: x ,y coordinates in lists as meters
     """
     R_E = 6.3781 * 10 ** 6
     lat2meter = 111.3199 * 10 ** 3
@@ -36,7 +41,13 @@ def gps2meter(long, lat):
 
 
 def curvature_cal(x, y):
+    """
+    Calculates cumulative travel, curvature of each segment and components of curvature vectors.
 
+    :param x: x coordinates list
+    :param y: y coordinates list
+    :return: Cumulative travel, curvatures with sign and k_urvature vectors
+    """
     z = np.zeros(len(x))
 
     track = np.transpose(np.array([x, y, z]))
@@ -88,15 +99,24 @@ def plot(x, y, k):
     return None
 
 
-def main():
-    long, lat = np.loadtxt(INPUT, delimiter='\t', unpack=True)
+if __name__ == "__main__":
+
+    long, lat = np.loadtxt(args.input, delimiter='\t', unpack=True)
     x, y = gps2meter(long, lat)
     L, curvature, k = curvature_cal(x, y)
 
     data_to_save = np.array([curvature, L])
     data_to_save = data_to_save.T
-    np.savetxt(filename, data_to_save, fmt='%.6f', delimiter=' ')
-    plot(x, y, k)
 
+    if not args.output:  # Output filename
+        filename = args.input.split('.')[0]+'_curv.txt'
+    else:
+        filename = args.output
 
-main()
+    try:  # Data save
+        np.savetxt(filename, data_to_save, fmt='%.6f', delimiter=' ')
+    except OSError:
+        print('Error while saving the data check the output filename if you have specified one.')
+
+    if args.plot:  # Vector plot
+        plot(x, y, k)
